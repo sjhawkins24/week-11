1327ff911533e9cdcb40be59714a386f81f28af9
Serena Hawkins <sjhawkins@ap.org>
2025-11-10 18:36:31 -0500

    Taking mean

--- apputil.py
+++ apputil.py
@@ -37,9 +37,11 @@ def kmeans_diamonds(n, k):
 
 def kmeans_timer(n, k, n_iter = 5):
     """Function to check the timing of the kmeans_diamond function"""
+    print(k)
     t = []
     for i in range(n_iter):
         start = time()
         kmeans_diamonds(n, k)
         t.append(time() - start)
-    return(t)
+        t_mean = sum(t)/len(t)
+    return(t_mean)
